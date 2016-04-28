aa82353320484abd27289fd01bfd53dec74e4e4a
ImYiZheng <yizhengsama@gmail.com>
2016-04-27 21:17:14 -0700

    Update pixel_limit.m

--- Project#4/pixel_limit.m
+++ Project#4/pixel_limit.m
@@ -1,8 +1,8 @@
 function legal_pixel = pixel_limit(origin_pixel, nr, nc)
 % PIXEL_LIMIT limits the pixel within legal region
 
-% Written by Qiong Wang at University of Pennsylvania
-% Oct. 9th, 2013
+% by YI ZHENG at USC
+
 
 legal_pixel = round(bsxfun(@rdivide, origin_pixel, origin_pixel(end, :)));
 legal_pixel = bsxfun(@min, bsxfun(@max, legal_pixel, [1 1 1]'), [nc, nr, 1]');  
